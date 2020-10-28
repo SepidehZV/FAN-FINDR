a5a9d4297662019a5db5b5b4a06b250039643393
Sepideh-tech <67478554+Sepideh-tech@users.noreply.github.com>
2020-10-27 21:21:04 -0400

    add venue to state for owner login & set alerts for login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,38 +1,60 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import './Login.scss';
-import loginImg from '../imgLogin.jpg'
+import loginImg from '../imgLogin.jpg';
+import { Alert } from 'react-bootstrap';
 import axios from 'axios';
 import SetStateContext from '../SetStateContext';
 
 export default function Login() {
-    const setState = useContext(SetStateContext);
-    
-    const history = useHistory();
-    const { state } = useLocation();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const setState = useContext(SetStateContext);
 
-    const handleLogin = (event) => {
-        event.preventDefault();
-        axios
-            .post('http://localhost:3001/api/auth/login', { email, password })
+  const history = useHistory();
+  const { state } = useLocation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alert, setAlert] = useState('');
+  useEffect(() => {
+    if (!alert) {
+      return;
+    }
+    document.querySelector('html').scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }, [alert]);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:3001/api/auth/login', { email, password })
+      .then(res => {
+        console.log(res.data)
+        localStorage.setItem('token', res.data.token);
+        history.push((state && state.from) || '/');
+        const user = res.data.user;
+        if (user.user_type) {
+          const venueId = state.venues.find(venue => venue.owner_id === user.id).id;
+          axios
+            .get(`http://localhost:3001/api/venues/:${venueId}`)
             .then(res => {
-                console.log(res.data)
-                localStorage.setItem('token', res.data.token);
-                history.push((state && state.from) || '/');
-                setState((prev) => ({...prev, user_type:res.data.user.user_type, user:res.data.user}));
+              const venue = res.data.venue;
+              return setState((prev) => ({ ...prev, user, user_type: user.user_type, venue }));
             })
+        }
+        setState((prev) => ({ ...prev, user_type: res.data.user.user_type, user: res.data.user }));
+      })
 
-            .catch(error => {
-                console.log(error);
+      .catch(error => {
+        console.log(error.response.data);
+        setAlert(error.response.data);
 
-            });
-        
-    };
-    return (
+      });
 
-      <div class="container-fluid">
+  };
+  return (
+
+    <div class="container-fluid">
       <div class="row no-gutter">
         <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
         <div class="col-md-8 col-lg-6">
@@ -40,31 +62,35 @@ export default function Login() {
             <div class="container">
               <div class="row">
                 <div class="col-md-9 col-lg-8 mx-auto">
-                <h1 className="card-title text-center"><span className="text-color">F</span>AN <span className="text-color">F</span>INDR</h1>
-                <hr className="seprating" />
-                <h5 className="text-center pb-4">Find your best team near you</h5>                  
-                <form onSubmit={handleLogin} >
+                  {alert &&
+                    <Alert variant='danger'>
+                      {alert}
+                    </Alert>}
+                  <h1 className="card-title text-center"><span className="text-color">F</span>AN <span className="text-color">F</span>INDR</h1>
+                  <hr className="seprating" />
+                  <h5 className="text-center pb-4">Find your best team near you</h5>
+                  <form onSubmit={handleLogin} >
                     <div class="form-label-group">
-                      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} required autofocus/>
+                      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} required autofocus />
                       <label for="inputEmail">Email address</label>
                     </div>
-    
+
                     <div class="form-label-group">
-                      <input type="password" id="inputPassword" class="form-control" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}  required/>
+                      <input type="password" id="inputPassword" class="form-control" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                       <label for="inputPassword">Password</label>
                     </div>
-    
+
                     <div class="custom-control custom-checkbox mb-3">
-                      <input type="checkbox" class="custom-control-input" id="customCheck1"/>
+                      <input type="checkbox" class="custom-control-input" id="customCheck1" />
                       <label class="custom-control-label" for="customCheck1">Remember password</label>
                     </div>
                     <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Sign in</button>
                     <div class="custom-control custom-checkbox mb-3">
-                       <a class="small" href="#">Forgot password?</a>
-                      </div>
+                      <a class="small" href="#">Forgot password?</a>
+                    </div>
 
-                      <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit"><Link to='/register/patron' className="link"> Create a Patron account</Link></button>
-                      <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit"> <Link to='/register/owner' className="link"> Create an owner/manager account</Link></button>
+                    <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit"><Link to='/register/patron' className="link"> Create a Patron account</Link></button>
+                    <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit"> <Link to='/register/owner' className="link"> Create an owner/manager account</Link></button>
 
                   </form>
                 </div>
@@ -74,5 +100,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-    )
+  )
 }
