a4fb1bd1699c12bf749383ee9caf4dc8810aa0b6
Sepideh-tech <67478554+Sepideh-tech@users.noreply.github.com>
2020-11-01 17:37:34 -0500

    hover for avatar pics added

--- client/src/components/Profile/Avatar.js
+++ client/src/components/Profile/Avatar.js
@@ -14,21 +14,28 @@ export default function Avatar(props) {
     event.preventDefault();
     props.onCancel();
   }
+  const selectAvatar = (event) => {
+    if (event.target.class === "img-circle") {
+      event.target.class = "img-circle avatar-pic"
+    }
+    
+    setAvatar(event.target.src)
+  }
   return(
     <div class="form-group-avatars">
     <div class="thumbnail">
-      <img src="images/avatars/avatar-1.png" class="img-circle avatar-pic" alt="avatar-1" onClick={ev => setAvatar(ev.target.src)}/>
-      <img src="images/avatars/avatar-2.png" class="img-circle" alt="avatar-2" onClick={ev => setAvatar(ev.target.src)}/>
-      <img src="images/avatars/avatar-3.png" class="img-circle" alt="avatar-3" onClick={ev => setAvatar(ev.target.src)}/>
-      <img src="images/avatars/avatar-4.png" class="img-circle" alt="avatar-4" onClick={ev => setAvatar(ev.target.src)}/>
-      <img src="images/avatars/avatar-5.png" class="img-circle" alt="avatar-5" onClick={ev => setAvatar(ev.target.src)}/>
-      <img src="images/avatars/avatar-6.png" class="img-circle" alt="avatar-6" onClick={ev => setAvatar(ev.target.src)}/>
+      <img src="images/avatars/avatar-1.png" class="img-circle avatar-pic" alt="avatar-1" onClick={selectAvatar}/>
+      <img src="images/avatars/avatar-2.png" class="img-circle avatar-pic" alt="avatar-2" onClick={selectAvatar}/>
+      <img src="images/avatars/avatar-3.png" class="img-circle avatar-pic" alt="avatar-3" onClick={selectAvatar}/>
+      <img src="images/avatars/avatar-4.png" class="img-circle avatar-pic" alt="avatar-4" onClick={selectAvatar}/>
+      <img src="images/avatars/avatar-5.png" class="img-circle avatar-pic" alt="avatar-5" onClick={selectAvatar}/>
+      <img src="images/avatars/avatar-6.png" class="img-circle avatar-pic" alt="avatar-6" onClick={selectAvatar}/>
       
       
     </div>
     <div className="btn-avatars">
-      <button type="submit" class="btn btn-success" onClick={handleSave}>Save</button>
-      <button type="submit" class="btn btn-danger"onClick={handleCancel}>Cancel</button>
+      <button type="submit" class="btn btn-success edit-btn" onClick={handleSave}>Save</button>
+      <button type="submit" class="btn btn-danger edit-btn"onClick={handleCancel}>Cancel</button>
     </div>
     
     </div>
