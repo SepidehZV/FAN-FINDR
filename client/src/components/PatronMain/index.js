e5e1c6cbf1b2eab5b56df34a7c90512ac1f6426a
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2020-11-04 18:52:02 -0500

    delete the map from the main page

--- client/src/components/PatronMain/index.js
+++ client/src/components/PatronMain/index.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 
 import './index.scss';
-import EventList from './EventList';
+import Event from './Event';
 import SearchBar from './SearchBar';
 import StateContext from '../../StateContext';
 import NavigationBar from '../NavigationBar';
@@ -9,6 +9,23 @@ import Map from './Map'
 
 export default function PatronMain(props) {
   const state = useContext(StateContext);
+  const eventList = state.events.map(event => (
+    <Event
+        key={event.id}
+        id={event.id}
+        venue_id={event.venue_id}
+        event_description={event.event_description}
+        event_name={event.event_name}
+        venue_name={event.venue_name}
+        team_name={event.team_name}
+        offers={event.offers}
+        venue_logo_url={event.venue_logo_url}
+        start_date={event.start_date}
+        addFav={props.addFav}
+        removeFav={props.removeFav}
+        sport_name={event.sport_name}
+      />
+  ))
   return (
     <div>
      
@@ -16,8 +33,8 @@ export default function PatronMain(props) {
     <SearchBar addFav={props.addFav} removeFav={props.removeFav}/>
     {/* <EventList events={state.events} teams={state.teams} /> */}
     <main role="main" className="container">
-
-<div className="mapimg"> <Map />  </div>
+    {/* {eventList} */}
+{/* <div className="mapimg"> <Map />  </div> */}
     
          </main>
   </div>
